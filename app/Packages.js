6c10355bdc1e0655bd4c7949d49e7d8b39640af2
Jaanulittleluv <134802505+Jaanulittleluv@users.noreply.github.com>
2024-11-30 19:29:24 +0530

    packages page created inside the app folder

---
+++ app/Packages.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './MyComponent.module.css';
+import { View, Button } from 'react-native-web';
+
+const Packages = () => {
+  return (
+    <View>
+        <div className={styles.packages}>
+                <div className={styles.box}>
+                <h1>$10/mo.</h1>
+                    <div className={styles.ribbonb}>
+                        <h2>Basic</h2>
+                    </div>
+                    <Button title="Get"/>
+                </div>
+                <div className={styles.box}>
+                <h1>$20/mo.</h1>
+                    <div className={styles.ribbong}>
+                        <h2>Gold</h2>
+                    </div>
+                    <Button title="Get"/>
+                </div>
+                <div className={styles.box}>
+                <h1>$30/mo.</h1>
+                    <div className={styles.ribbonp}>
+                        <h2>Premium</h2>
+                    </div>
+                    <Button title="Get"/>
+                </div>
+        </div>
+    </View>
+  );
+};
+
+export default Packages;
